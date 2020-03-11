cf2a4574c0c71ce9c17b1019e34921c589157fb7
JasonAspire <jason.lehmann@aspiritech.org>
2020-03-11 11:52:44 -0500

    Completed phase 5

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -1,8 +1,11 @@
 //Start point for the client side application
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {BrowserRouter } from 'react-router-dom'
 
-import Home from './components/Home'
+import Routes from './Routes'
+ReactDOM.hydrate(<BrowserRouter>
 
-ReactDOM.hydrate(<Home/>, document.querySelector('#root') );
+<Routes/>
+</BrowserRouter>, document.querySelector('#root') );
 
